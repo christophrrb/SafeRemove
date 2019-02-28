1249945f30bfc8620179757d88d8740e7bc65341
Christopher <cballenger@steamsport.com>
2019-02-28 10:08:55 -0500

    The trash directory is now set by asking the user to input it if it doesn't exist, or it gets the trash directory from a serialized Information object. There is also a command to set/change the trash directory, --set-directory. Some variable names were changed too.

--- SafeRemove.java
+++ SafeRemove.java
@@ -1,33 +1,99 @@
-import java.io.File;
+import java.io.*;
+import java.util.Scanner;
 
 public class SafeRemove {
 
 	public static void main(String[] args) {
-		if (args[0].equals("")) {
-			System.out.println("Please enter a file path.");
-			System.exit(0);
+		try {
+			switch (args[0]) {
+
+				case "--set-directory":
+						
+					File infoFile = new File("information.ser");
+				
+					if (infoFile.exists()) {
+						FileInputStream fis = new FileInputStream(infoFile);
+						ObjectInputStream ois = new ObjectInputStream(fis);
+
+						Information serializedObject = (Information) ois.readObject();
+
+						String originalTrashDirectory = serializedObject.getTrashDirectory();
+
+						serializedObject.setTrashDirectory(args[1]);
+	
+						FileOutputStream fos = new FileOutputStream(infoFile);
+						ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+						oos.writeObject(serializedObject);
+
+						System.out.printf("The trash directory was successfully changed from \"%s\" to \"%s\"", originalTrashDirectory, args[1]);
+
+						ois.close();
+						fis.close();
+
+						System.exit(1);
+					} else {
+						//To be written
+					}
+
+				break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
 		try {
-			File file = new File(args[0]);
-
-			if (file.exists() == false) {
-				System.out.println(file.getPath() + " does not exist.");
-				System.exit(0);
+			File file = new File(args[0]); //Create a new File object based off of the given file name.
+			
+			if (file.exists() == false) { //If the file is not found,
+				System.out.println(file.getPath() + " does not exist."); //Print the file path and tell the user that a file does not exist there,
+				System.exit(0); //and close the program.
 			}
 
-			File trash = new File("/home/christophrrb/.local/share/Trash/files/" + file.getName());
-			String trashPath = trash.getPath();
+			//Checking for the serialized object with the trash directory.
+			File infoFile = new File("information.ser");
+			File trashDirectory;
 
-			for (int i = 1; trash.exists() && i <= 100; i++) {
-				String newFileName = trashPath + "(" + i + ")";
+			if (infoFile.exists()) {
+				FileInputStream fis = new FileInputStream(infoFile);
+				ObjectInputStream ois = new ObjectInputStream(fis);
 
-				trash = new File(newFileName);
+				Information serializedObject = (Information) ois.readObject();
+				trashDirectory = new File(serializedObject.getTrashDirectory());
+
+				ois.close();
+				fis.close();
+			} else {
+				System.out.println("Enter the trash directory");
+				
+				Scanner sc = new Scanner(System.in);
+				String userTrashDirectory = sc.nextLine();
+
+				Information info = new Information(userTrashDirectory);
+				
+				FileOutputStream fos = new FileOutputStream(infoFile);
+				ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+				oos.writeObject(info);
+
+				trashDirectory = new File(userTrashDirectory);
+
+				System.out.printf("The trash directory \"%s\" was successfully saved.\n", userTrashDirectory);
 			}
 
 
-				file.renameTo(trash);
-				System.out.println("File was moved to trash with the name " + trash.getName() + ". (" + trash.getPath() + ")");
+			File toTrash = new File(trashDirectory + File.separator + file.getName());
+			//String trashPath = trash.getPath();
+
+			for (int i = 1; toTrash.exists() && i <= 100; i++) {
+				String newFileName = trashDirectory + File.separator + file.getName() + "(" + i + ")";
+
+				toTrash = new File(newFileName);
+			}
+
+
+				file.renameTo(toTrash);
+				System.out.println("File was moved to trash with the name " + toTrash.getName() + ". (" + toTrash.getPath() + ")");
 
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Please enter a file.");
